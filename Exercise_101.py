67ad79ca6158476bfc12f7c35aa4cc1fa1806c8e
Hamza El Aouane <haouane@spartaglobal.com>
2020-02-05 16:28:51 +0000

    Exercise 101 completed

---
+++ Exercise_101.py
@@ -0,0 +1,31 @@
+# List basics :D
+
+
+#8 - Re assign an item and print all of the list again
+
+#1 - Define a bucket list with 10 items
+bucket_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#2 - Print the entire list
+print(bucket_list)
+
+#3 - Check the length of the list / how many items inside the list?
+lenght_list = len(bucket_list)
+print(lenght_list)
+
+#4 - print the first item
+print(bucket_list[0])
+
+#5 - Prin the second item
+print(bucket_list[1])
+
+#6 - add one more item to the list
+bucket_list.append('10')
+print(bucket_list)
+
+#7 - prin the last item in the list
+print(bucket_list[-1])
+
+#8 - Re assign an item and print all of the list again
+bucket_list[10] = 13
+print(bucket_list)
